4ba3240742f4d0cdad877134b72aa1c0e439a49f
Lou Punit <1158984+loopunit@users.noreply.github.com>
2022-01-13 23:01:50 -0800

    Initial commit

---
+++ test/source/greeter.cpp
@@ -0,0 +1,18 @@
+#include <doctest/doctest.h>
+#include <greeter/greeter.h>
+
+#include <string>
+
+TEST_CASE("Greeter") {
+  using namespace greeter;
+
+  Greeter greeter("Tests");
+
+  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
+  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
+  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+}
+
+TEST_CASE("Greeter version") {
+}
